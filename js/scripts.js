168cf543560349a135b5d723c39b10bb7755f60c
Misael703 <misael.ocas@gmail.com>
2021-04-09 22:19:24 -0400

    ola

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,35 @@
 function mensaje() {
-    var nombre =  document.getElementById('txtNombre').value;
-    alert('Yo soy: '+ nombre)
+  var nombre = document.getElementById("txtNombre").value;
+  alert("Yo soy: " + nombre);
+}
+
+function validarRut() {
+  var rut = document.getElementById("txtRut").value;
+  if (rut.length != 10) {
+    alert("Largo del rut incorrecto");
+    return false;
   }
+  var num = 3;
+  var suma = 0;
 
-function validarRut{
-    var rut = document.getElementById('txtRut').value;
-    if (rut.length != 10){
-        alert('Largo del rut incorrecto');
-        return false;
+  for (let index = 0; index < 8; index++) {
+    var carac = rut.slice(index, index + 1);
+    //alert(carac + ' x ' + num)//
+    suma = suma + carac * num;
+    num = num - 1;
+    if (num == 1) {
+      num = 7;
     }
-    var num=3;
-    var suma=0;
-
-    for (let index = 0; index < 8; index++) {
-        var carac= rut.slice(index,index+1);
-       //alert(carac + ' x ' + num)//
-        suma= suma+(carac*num);
-        num=num-1;
-        if (num == 1){
-            num=7;
-        }
-        
+  }
+  //alert('El total es: '+ suma)//
+  var resto = suma % 11;
+  var dv = 11 - resto;
+  if (dv > 9) {
+    if (dv == 10) {
+      dv = "K";
+    } else {
+      dv = 0;
     }
-    //alert('El total es: '+ suma)//
-    var resto=suma % 11;
-    var dv= 11 - resto;
-    if(dv > 9 ){
-        if(dv==10){
-            dv="K";
-        }
-        else{
-            dv=0
-        }
-    }
-    alert('El DV es: ' + dv);
+  }
+  alert("El DV es: " + dv);
 }
